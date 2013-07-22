9c3a40ce1cb8b7ef458f5b9680d8b593f23d6deb
Ross Miller <rgmiller@ornl.gov>
2013-07-22 14:35:35 -0400

    Changed how the simple_deploy script creates the new db file and also the selinux context it applies to the files.

--- simple_deploy.sh
+++ simple_deploy.sh
@@ -17,15 +17,27 @@ do
 	fi
 done
  
-cp manage.py $DEST
-
-
 # Execute 'manage.py syncdb' to create a new database file
-pushd $DEST
+# Note: For reasons that remain unclear, trying to execute
+# syncdb in the $DEST dir doesn't work (the db file isn't
+# created), so we execute it in the current directory
+# and then move it to $DEST
+SAVEFILE=sqlite.db.save.`date +%s`
+if [ -a sqlite.db ]
+then
+	mv sqlite.db $SAVEFILE
+fi
 /usr/bin/python manage.py syncdb
-rm manage.py  # don't need it once the db is created
-popd
+mv sqlite.db $DEST
+
+if [ -a $SAVEFILE ]
+then
+	mv $SAVEFILE sqlite.db
+fi
 
 
+# Update permissions and selinux context so httpd can access the files
 chown -R apache.apache $DEST
-chcon -R unconfined_u:object_r:httpd_sys_content_t:s0 $DEST
+chcon -R unconfined_u:object_r:httpd_sys_content_rw_t:s0 $DEST
+
+echo "Don't forget to update the DB path in settings.py!"
