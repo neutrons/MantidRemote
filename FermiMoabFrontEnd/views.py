454794527fdff66b7c72ad35162c3e56a5ccba3d
Ross Miller <rgmiller@ornl.gov>
2013-07-25 15:02:23 -0400

    Added code to return error messages when the view functions encounter an error.

--- FermiMoabFrontEnd/views.py
+++ FermiMoabFrontEnd/views.py
@@ -47,15 +47,15 @@ def transaction( request):
     
     # Validate the request
     if request.method != 'GET':
-        return HttpResponse( status=400)  # Bad request
+        return HttpResponse( err_not_get(),  status=400)  # Bad request
     
     if not 'Action' in request.GET:
-        return HttpResponse( status=400)  # Bad request
+        return HttpResponse( err_missing_param( 'Action'), status=400)  # Bad request
     
     action = request.GET['Action']
     if not (action == 'Start' or action == 'Stop'):
-        return HttpResponse( status=400)
-    
+        return HttpResponse( json_err_msg("Expected either 'Start' or 'Stop' for the 'Action' parameter."),
+                             status=400)
     if action == 'Start':
         # Start a new transaction
         new_trans = Transaction()
@@ -77,7 +77,8 @@ def transaction( request):
             # ourselves
             recursive_rm( dir_name)
             new_trans.delete()
-            return HttpResponse( status=500) # internal server error
+            return HttpResponse( json_err_msg( "Cannot start transaction: Failed to set ACL's on transaction directory."),
+                                 status=500) # internal server error
 
         # If we make it here, everything's good - save the transaction object
         # to the DB and return the required JSON        
@@ -93,8 +94,9 @@ def transaction( request):
         # Stop an existing transaction
         # Need to specify the transaction ID
         (trans, error_response) = validate_trans_id( request)
-        if not error_response == None:
-            return HttpResponse( status=400)  # Bad request
+        if error_response != None:
+        # TransID didn't validate...
+            return error_response
         
         # The transaction is validated - delete the files/directories
         recursive_rm( trans.directory)
@@ -124,10 +126,10 @@ def download( request):
     # Verify the that we have the proper request parameters
     # Validate the request
     if request.method != 'GET':
-        return HttpResponse( status=400)  # Bad request
+        return HttpResponse( err_not_get(), status=400)  # Bad request
     
     if not 'File' in request.GET:
-        return HttpResponse( status=400)  # Bad request
+        return HttpResponse( err_missing_param( 'File'), status=400)  # Bad request
     
     (trans, error_response) = validate_trans_id( request)
     if error_response != None:
@@ -156,10 +158,10 @@ def upload( request):
     
     # Verify the request method, the FILES dict and TransID parameter
     if request.method != 'POST':
-        return HttpResponse( status=400) # Bad request
+        return HttpResponse( err_not_post(), status=400) # Bad request
     
     if len(request.FILES.keys()) == 0:
-        return HttpResponse( status=400)
+        return HttpResponse( json_err_msg( "No files specified to upload"), status=400)
     
     (trans, error_response) = validate_trans_id( request)
     if error_response != None:
@@ -182,7 +184,7 @@ def files( request):
     '''
     # Verify the that we have the proper request parameters
     if request.method != 'GET':
-        return HttpResponse( status=400)  # Bad request
+        return HttpResponse( err_not_get(), status=400)  # Bad request
     
     (trans, error_response) = validate_trans_id( request)
     if error_response != None:
@@ -297,23 +299,26 @@ def validate_trans_id( request):
     # Need to specify the transaction ID
     if request.method == 'GET':
         if not 'TransID' in request.GET:
-            return (None, HttpResponse( status=400))  # Bad request
+            return (None, HttpResponse( err_missing_param( 'TransID'), status=400))  # Bad request
         trans_id = request.GET['TransID']
     else:
         if not 'TransID' in request.POST:
-            return (None, HttpResponse( status=400))  # Bad request
+            return (None, HttpResponse( err_missing_param( 'TransID'), status=400))  # Bad request
         trans_id = request.POST['TransID']
     
     try:        
         trans = Transaction.objects.get( id=trans_id)
     except Transaction.DoesNotExist:
-        return (None, HttpResponse( status=400))  # Bad request
+        return (None, HttpResponse( json_err_msg( "Transaction '%s' does not exist"%trans_id),
+                                    status=400))  # Bad request
             
     # verify the id we're trying to stop is owned by the user
     if trans.owner != request.user:
-        return (None, HttpResponse( status=400))  # Bad request
+        return (None, HttpResponse( json_err_msg( "Transaction '%s' is not owned by the current user"%trans_id),
+                                    status=400))  # Bad request
     
     return (trans, None)
+    
     
 def json_err_msg( msg):
     '''
@@ -322,4 +327,13 @@ def json_err_msg( msg):
     # The name 'Err_Msg' is defined by the API doc
     out = { "Err_Msg" : msg}
     return json.dumps( out) 
-    
+
+# JSON output for some standard, oft-used error messages
+def err_not_get():
+    return json_err_msg("This URL only accepts 'GET' requests")
+
+def err_not_post():
+    return json_err_msg("This URL only accepts 'POST' requests")
+
+def err_missing_param( param):
+    return json_err_msg("Missing required parameter '%s'"%param)
